7ad94b1e92b5479b6f0c87f9d97b571b449b3ca8
mohamed <az.galal666@gmail.com>
2022-04-30 06:49:22 -0700

    add sports collectionView

---
+++ SportsApp/SportsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SportsCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Mohamed Galal on 26/04/2022.
+//
+
+import UIKit
+
+class SportsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lableView: UILabel!
+}
