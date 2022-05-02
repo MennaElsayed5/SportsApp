d9a497769c61b6e8c721c63e23b32d5dd5c7fa08
MennaElsayed5 <mennaaaaelsayed@gmail.com>
2022-05-02 22:34:21 +0200

    handle packages

---
+++ SportsApp/Constant/URLs.swift
@@ -0,0 +1,11 @@
+//
+//  URLs.swift
+//  SportsApp
+//
+//  Created by Menna on 02/05/2022.
+//
+
+import Foundation
+struct URLs{
+    
+}
---
+++ SportsApp/Model/RemoteData/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkServise.swift
+//  SportsApp
+//
+//  Created by Menna on 02/05/2022.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkService {
+    
+}
